6ed1079764bf2129e13e1424df2b9b662e333289
Nedal <nedalosama3@gmail.com>
2024-07-25 20:34:05 +0300

    Generics

--- advanced_1/Program.cs
+++ advanced_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using advanced_1.Bubble_Sort;
+using advanced_1.Generic;
 
 namespace advanced_1
 {
@@ -9,15 +10,28 @@ namespace advanced_1
         static void Main(string[] args)
         {
             #region BubbleSort
-           /* int[] array = { 64, 34, 25, 12, 22, 11, 90 };
-            BubbleSort.Bubble_Sort(array);
+            /* int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+             BubbleSort.Bubble_Sort(array);
 
-            Console.WriteLine("Sorted array:");
-            foreach (int item in array)
-            {
-                Console.Write(item + " ");
-            }*/
+             Console.WriteLine("Sorted array:");
+             foreach (int item in array)
+             {
+                 Console.Write(item + " ");
+             }*/
             #endregion
+
+
+            #region Generics
+
+            Range<int> intRange = new Range<int>(1, 10);
+            Console.WriteLine(intRange.IsInRange(5)); 
+            Console.WriteLine(intRange.Length()); 
+
+            Range<double> doubleRange = new Range<double>(1.0, 10.0);
+            Console.WriteLine(doubleRange.IsInRange(5.5)); 
+            Console.WriteLine(doubleRange.Length()); 
+            #endregion
+
         }
 
     }
