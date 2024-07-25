c4928171d87bbe0877ae638006a67f7d1412d0f7
Nedal <nedalosama3@gmail.com>
2024-07-25 20:25:04 +0300

    BubbleSort

---
+++ advanced_1/Bubble_Sort/BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advanced_1.Bubble_Sort
+{
+    public class BubbleSort
+    {
+        public static void Bubble_Sort<T>(T[] array) where T : IComparable<T>
+        {
+            int n = array.Length;
+            bool swapped;
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                      
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                
+                if (!swapped)
+                    break;
+
+            }
+        }
+    }
+
+}
--- advanced_1/Program.cs
+++ advanced_1/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace advanced_1
+﻿
+
+using advanced_1.Bubble_Sort;
+
+namespace advanced_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region BubbleSort
+           /* int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+            BubbleSort.Bubble_Sort(array);
+
+            Console.WriteLine("Sorted array:");
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }*/
+            #endregion
         }
+
     }
 }
